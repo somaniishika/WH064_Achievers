0e47bc195f7788d76973b521352d453c5fcef77d
vidhijaiswal-2702 <110765626+vidhijaiswal-2702@users.noreply.github.com>
2024-04-06 13:37:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -39,3 +39,28 @@ function fetchFeaturedCourses() {
 window.onload = function() {
     fetchFeaturedCourses();
 };
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var section = document.querySelector(".special-section");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the section, open the modal
+section.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
